628573f48943a8b76194d280396ce3f2d3a4343c
Stallos11 <vincentdelev@yahoo.fr>
2023-01-12 11:03:14 +0100

    fix: production mode

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,5 +4,4 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicDir: process.env.VITE_ENV === 'production' ? '/Mealaway/' : '/',
 });
