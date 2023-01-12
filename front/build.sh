8ea7bcc0c24cc0c8ecd61524b2783884c35c1953
Stallos11 <vincentdelev@yahoo.fr>
2023-01-12 11:07:20 +0100

    fix: build script

--- front/build.sh
+++ front/build.sh
@@ -15,6 +15,8 @@ echo > .nojekyll
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+git config --global user.email "vincent.dimarco1@gmail.com"
+git config --global user.name "Vincentdm31"
 
 git init
 git checkout -B main
