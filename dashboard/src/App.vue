cc830c42993669f4a098a2304eb268d475792277
Stallos11 <vincentdelev@yahoo.fr>
2023-01-13 15:23:33 +0100

    feat: deployment

--- dashboard/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <Layout>
-    <router-view></router-view>
-  </Layout>
-</template>
-
-<script setup lang="ts">
-import Layout from './components/Layout.vue'
-</script>
--- dashboard/src/views/Dashboard.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    Dashboard
-</template>
-  
-<script setup lang="ts">
-</script>
-  
