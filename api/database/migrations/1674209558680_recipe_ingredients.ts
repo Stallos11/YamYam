b1353fbf833c311124d7a848b074a89a92904598
Stallos11 <vincentdelev@yahoo.fr>
2023-01-20 12:46:13 +0100

    fix: recipe relation

--- api/database/migrations/1674209558680_recipe_ingredients.ts
+++ api/database/migrations/1674209558680_recipe_ingredients.ts
@@ -10,7 +10,7 @@ export default class extends BaseSchema {
 
       // Relations
       table.uuid("recipe_id").references("recipes.id");
-      table.uuid("ingredient_id").references("recipes.id");
+      table.uuid("ingredient_id").references("ingredients.id");
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
