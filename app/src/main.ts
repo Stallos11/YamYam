ba37a7e9610d7e16267aab16108322ccf1026e41
Stallos11 <vincentdelev@yahoo.fr>
2023-01-26 15:09:49 +0100

    feat: vuejs starter app

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,10 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import VueAxentix from "@axentix/vue";
+import { createApp } from 'vue';
+import { router } from './router/router';
+import App from './App.vue';
+import VueAxentix from '@axentix/vue';
 
 const app = createApp(App);
 app.use(VueAxentix);
-app.mount("#app");
+app.use(router);
+
+app.mount('#app');
