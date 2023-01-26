e3f07b4de95c6c049ddb984906fd8e0eff2d2ea6
Stallos11 <vincentdelev@yahoo.fr>
2023-01-26 14:13:02 +0100

    feat: deployment

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: process.env.NODE_ENV === 'production' ? '/YamYam/' : '/',
+  base: '/YamYam/',
   plugins: [
     vue(),
     VitePWA({
