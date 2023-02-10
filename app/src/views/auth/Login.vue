3a6621b74a7fa950bc15521fe8f337b8a3b5bdfa
Stallos11 <vincentdelev@yahoo.fr>
2023-02-10 17:16:40 +0100

    feat: captcha key

--- app/src/views/auth/Login.vue
+++ app/src/views/auth/Login.vue
@@ -1,72 +1,65 @@
 <template>
-  <div class="card bg-light rounded-2 overflow-visible mt-4 light-shadow-4 mb-4">
-    <div class="robot"></div>
-    <h1 class="card-header text-center my-2 font-s5 font-w500">Login</h1>
+  <div class="h100 d-flex fx-center vcenter auth-card">
+    <div class="card bg-light rounded-2 overflow-visible mt-4 light-shadow-4">
+      <h1 class="card-header text-center my-2 font-s5 font-w500">Login</h1>
 
-    <div class="card-content">
-      <div class="text-center">
-        <router-link to="/register" class="text-primary font-w500">Don't have an account ? Create it now !</router-link>
-      </div>
-
-      <ax-form ref="form" class="mt-4" @keypress.enter.prevent="login">
-        <ax-form-field label="Email*">
-          <ax-form-control type="email" v-model="user.email" lazy :rules="[required, emailRule]"></ax-form-control>
-        </ax-form-field>
-
-        <ax-form-field label="Password*">
-          <ax-form-control type="password" v-model="user.password" lazy :rules="[required, passwordRule]"></ax-form-control>
-        </ax-form-field>
-      </ax-form>
-
-      <div class="text-center mt-3">
-        <router-link to="/forgot-password" class="text-grey text-light-2">Forgot password ?</router-link>
-      </div>
-
-      <div class="d-flex fx-center my-4 captcha">
-        <VueRecaptcha
-          ref="captcha"
-          :sitekey="siteKey"
-          :load-recaptcha-script="true"
-          theme="dark"
-          @verify="handleSuccess"
-          @expired="handleExpired"
-          @error="handleError"
-        ></VueRecaptcha>
-      </div>
-
-      <div class="social-divider my-4">
-        <span></span>
-        <p>Or with</p>
-      </div>
-
-      <div class="d-flex fx-center social-auth-icons">
-        <ax-btn
-          class="rounded-2 text-white d-flex vcenter fx-center py-3 discord-bg shadow-1"
-          circle
-          @click="() => authStore.redirectWithSocial('discord')"
-        >
-          <Icon icon="akar-icons:discord-fill" size="50"></Icon>
-        </ax-btn>
-
-        <ax-btn class="rounded-2 white d-flex vcenter fx-center ml-3 shadow-1" circle @click="() => authStore.redirectWithSocial('google')">
-          <Icon icon="flat-color-icons:google" size="50"></Icon>
-        </ax-btn>
-      </div>
-    </div>
-
-    <div class="card-footer pb-0 px-0">
-      <ax-btn class="w100 primary d-flex fx-center vcenter" @click="login" :disabled="authStore.isLoading || !isNotRobot">
-        <div class="spinner spinner-small text-primary" v-if="authStore.isLoading">
-          <svg viewBox="25 25 50 50">
-            <circle class="spinner-path" cx="50" cy="50" r="20" fill="none" stroke-width="3" />
-          </svg>
+      <div class="card-content">
+        <div class="text-center">
+          <router-link to="/register" class="text-primary font-w500">Don't have an account ? Create it now !</router-link>
         </div>
 
-        <template v-else>
-          Sign In
-          <Icon icon="bx:log-in" class="ml-1"></Icon>
-        </template>
-      </ax-btn>
+        <ax-form ref="form" class="mt-4" @keypress.enter.prevent="login">
+          <ax-form-field label="Email*">
+            <ax-form-control type="email" v-model="user.email" lazy :rules="[required, emailRule]"></ax-form-control>
+          </ax-form-field>
+
+          <ax-form-field label="Password*">
+            <ax-form-control type="password" v-model="user.password" lazy :rules="[required, passwordRule]"></ax-form-control>
+          </ax-form-field>
+        </ax-form>
+
+        <div class="text-center mt-3">
+          <router-link to="/forgot-password" class="text-grey text-light-2">Forgot password ?</router-link>
+        </div>
+
+        <div class="d-flex fx-center my-4 captcha">
+          <VueRecaptcha
+            ref="captcha"
+            :sitekey="siteKey"
+            :load-recaptcha-script="true"
+            theme="dark"
+            @verify="handleSuccess"
+            @expired="handleExpired"
+            @error="handleError"
+          ></VueRecaptcha>
+        </div>
+
+        <div class="social-divider my-4">
+          <span></span>
+          <p>Or with</p>
+        </div>
+
+        <div class="d-flex fx-center social-auth-icons">
+          <ax-btn class="rounded-2 white d-flex vcenter fx-center shadow-1" circle @click="() => authStore.redirectWithSocial('google')">
+            <Icon icon="flat-color-icons:google" size="50"></Icon>
+          </ax-btn>
+        </div>
+      </div>
+
+      <div class="card-footer pb-0 px-0">
+        <ax-btn class="w100 primary d-flex fx-center vcenter" @click="login" :disabled="authStore.isLoading || !isNotRobot">
+          <div class="spinner spinner-small text-primary" v-if="authStore.isLoading">
+            <svg viewBox="25 25 50 50">
+              <circle class="spinner-path" cx="50" cy="50" r="20" fill="none" stroke-width="3" />
+            </svg>
+          </div>
+
+          <template v-else>
+            Sign In
+            <Icon icon="bx:log-in" class="ml-1"></Icon>
+          </template>
+        </ax-btn>
+      </div>
     </div>
   </div>
 </template>
