fd9895476724f9e57c247f3b7208d6b6bf04213c
Vincentdm31 <vincent.dimarco1@gmail.com>
2023-02-15 21:23:43 +0100

    wip: recipe preview

--- dashboard/src/components/RecipeCreate.vue
+++ dashboard/src/components/RecipeCreate.vue
@@ -156,7 +156,71 @@
         </div>
       </div>
     </ax-tab-item>
-    <ax-tab-item class="h100" id="tab4">Preview</ax-tab-item>
+    <ax-tab-item class="h100" id="tab4">
+      <div class="p-4">
+        <p class="font-s5">
+          {{ recipeStore.recipeCreate.recipe.name }}
+        </p>
+        <div class="">
+          <ax-btn class="btn primary px-4 py-1 rounded-3 font-s2 mr-3 mb-3">{{
+            recipeCategoryStore.recipe_categories.find(
+              (rc) => rc.id === recipeStore.recipeCreate.recipe.recipeCategoryId
+            )?.category
+          }}</ax-btn>
+          <ax-btn class="btn tertiary px-4 py-1 rounded-3 font-s2">{{
+            recipeTypeStore.recipe_types.find(
+              (rc) => rc.id === recipeStore.recipeCreate.recipe.recipeTypeId
+            )?.type
+          }}</ax-btn>
+        </div>
+        <div class="grix xs2 md12 gutter-xs5 font-s3 mt-4">
+          <div class="d-flex">
+            <Icon icon="ic:outline-access-time" class="mr-2"></Icon>
+            <span>{{ recipeStore.recipeCreate.recipe.preparationTime }}</span>
+          </div>
+          <div class="d-flex">
+            <Icon icon="icon-park-outline:cook" class="mr-2"></Icon>
+            <span>{{ recipeStore.recipeCreate.recipe.cookingTime }}</span>
+          </div>
+          <div class="d-flex">
+            <Icon icon="mdi:flame" class="mr-2"></Icon>
+            <span>{{ recipeStore.recipeCreate.recipe.difficulty }}</span>
+          </div>
+          <div class="d-flex">
+            <Icon icon="mdi:users-group" class="mr-2"></Icon>
+            <span>{{ recipeStore.recipeCreate.recipe.eatersAmount }}</span>
+          </div>
+        </div>
+        <p class="pb-5">
+          {{ recipeStore.recipeCreate.recipe.description }}
+        </p>
+        <p class="font-s3 mt-5 text-primary">Ingrédients</p>
+        <div class="divider white"></div>
+        <div
+          v-for="ingredient in recipeStore.recipeCreate.ingredients"
+          :key="ingredient.id" class="mt-5"
+        >
+          <p class="mb-0">{{ ingredient.product_name }}</p>
+          <p class="mt-0">
+            <span>{{ ingredient.quantity }}</span
+            ><span>{{ ingredient.unit }}</span>
+          </p>
+        </div>
+        <p class="font-s3 mt-5 text-primary">Instructions</p>
+        <div class="divider white"></div>
+        <div
+          v-for="instruction in recipeStore.recipeCreate.instructions"
+          :key="instruction.id" class="mt-5"
+        >
+          <p class="mb-0 font-s">
+            {{ instruction.title }}
+          </p>
+          <p class="mt-1">
+            {{  instruction.content }}
+          </p>
+        </div>
+      </div>
+    </ax-tab-item>
 
     <IngredientDetailModal />
   </ax-tab>
@@ -165,7 +229,6 @@
 import { ref, onBeforeMount } from "vue";
 import { useRecipeStore } from "../stores/recipes";
 import Timepicker from "../components/Timepicker.vue";
-import { useAuthStore } from "../stores/auth";
 import { useRecipeTypeStore } from "../stores/recipeTypes";
 import { useRecipeCategoryStore } from "../stores/recipeCategories";
 import IngredientSearch from "./IngredientSearch.vue";
@@ -175,7 +238,6 @@ import IngredientDetailModal from "../views/ingredients/IngredientDetailModal.vu
 const recipeStore = useRecipeStore();
 const recipeTypeStore = useRecipeTypeStore();
 const recipeCategoryStore = useRecipeCategoryStore();
-const authStore = useAuthStore();
 const categories = ref();
 const types = ref();
 const currentTab = ref(0);
